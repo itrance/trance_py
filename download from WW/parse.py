48caefc6327676d90eb1a027a9fd8af069db7852
itrance <itrance@users.noreply.github.com>
2020-03-08 18:08:45 +0300

    Create parse.py

---
+++ download from WW/parse.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+MAIN_URL_WALL_1 = 'http://wallpaperswide.com/page/'
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:73.0) Gecko/20100101 Firefox/73.0',
+           'Accept': '*/*'}
+start_page = int(input('Начинать со страницы: '))
+end_page = int(input('До страницы: '))
+
+
+def get_html(url, json=None):
+    r = requests.post(url)
+    return r
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    items = soup.find_all('div', class_='thumb')
+    pictures = []
+    for item in items:
+        with_dot_html = item.find('a').get('href')
+        pictures.append(
+            'http://wallpaperswide.com/download' + with_dot_html[:len(with_dot_html) - 16] + '-3840x2160.html')
+    for aaa in pictures:
+        if requests.get(aaa).status_code == 200 :
+            name_of_pict = aaa[35:len(aaa) - 5]#''.join(random.choice(ascii_letters) for i in range(15))
+            image_to_download = requests.get(aaa, stream=True)
+            fi = open(name_of_pict + '.jpg', 'wb')
+            fi.write(image_to_download.content)
+            image_to_download.close()
+            print(f'{aaa} успешно загружена')
+        else:
+            pass
+
+
+def parse():
+    global start_page
+    while start_page <= end_page:
+        MAIN_URL_WALL = MAIN_URL_WALL_1 + str(start_page)
+        html = get_html(MAIN_URL_WALL)
+        if html.status_code == 200:
+            get_content(html.text)
+            print(f'Сохранение картинок со страницы {start_page} завершено')
+            start_page += 1
+        else:
+            print(f'\nСтраницы нет')
+    print("Сохранение картинок закончено")
+
+
+parse()
